4cbd6dd58fe8574b6da2767437c4c1c8e1ba94e6
Roma <zohanblz@gmail.com>
2020-12-04 11:51:43 +0300

    add post

--- src/script.js
+++ src/script.js
@@ -1,10 +1,20 @@
-
 // eslint-disable-next-line import/no-unresolved
 import './style.scss';
+
+const requestUrl = 'http://jsonplaceholder.typicode.com/posts';
 
 const input = document.querySelector('.form__input');
 const button = document.querySelector('.form__button');
 const incorrect = document.querySelector('.form__incorrect');
+const sucsess = document.querySelector('.form__sucsess');
+
+const request = (url, body) => fetch(url, {
+  method: 'POST',
+  body: JSON.stringify(body),
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}).then((response) => response.json());
 
 const click = () => {
   button.classList.add('form__button_click');
@@ -13,12 +23,33 @@ const click = () => {
   }, 200);
 };
 
+const sucsessRequest = () => {
+  sucsess.classList.add('form__sucsess_true');
+  setTimeout(() => {
+    sucsess.classList.remove('form__sucsess_true');
+  }, 1000);
+};
+
+const incorrectData = () => {
+  incorrect.classList.add('form__incorrect_true');
+  setTimeout(() => {
+    incorrect.classList.remove('form__incorrect_true');
+  }, 1000);
+};
+
 const valid = () => {
   if (input.value === '') {
-    incorrect.classList.add('form__incorrect_true');
-    setTimeout(() => {
-      incorrect.classList.remove('form__incorrect_true');
-    }, 1000);
+    incorrectData();
+  } else {
+    const tel = {
+      tel: input.value,
+    };
+    request(requestUrl, tel)
+      .then(() => {
+        sucsessRequest();
+        input.value = '';
+      })
+      .catch((err) => { console.log(err); });
   }
 };
 
